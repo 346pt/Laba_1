1ced4f2b5581ccff85d19c66221922cc6bd75f5a
12 <12@DESKTOP-MJ57H2D>
2024-04-03 11:01:29 +0300

    Добавьте файлы проекта.

---
+++ Laba_1/Program.cs
@@ -0,0 +1,15 @@
+﻿double n = Convert.ToDouble(Console.ReadLine());
+double x = Convert.ToDouble(Console.ReadLine());
+double result = (Math.PI / 2);
+if (Math.Abs(x) > 1)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        result += (((Math.Pow(-1, n + 1)) / (2 * n + 1) * (Math.Pow(x, 2 * n + 1))));
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Значение X выходит за предел");
+}
